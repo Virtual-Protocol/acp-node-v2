9c430e2408ca0f905114e464a2af4bed6a2a7b19
Zuhwa <zuhwa@virtuals.io>
2026-04-08 14:40:36 +0800

    fix: update USDC and ACP contract addresses for baseSepolia and bscTestnet

--- src/core/constants.ts
+++ src/core/constants.ts
@@ -6,20 +6,20 @@ import { base, baseSepolia, bsc, bscTestnet } from "viem/chains";
 // ---------------------------------------------------------------------------
 
 export const USDC_ADDRESSES: Record<number, string> = {
-  [baseSepolia.id]: "0xB270EDc833056001f11a7828DFdAC9D4ac2b8344",
+  [baseSepolia.id]: "0xECc22a8F6fD62388498fBa19813E214605a2BDb3",
+  [bscTestnet.id]: "0xECc22a8F6fD62388498fBa19813E214605a2BDb3",
   [base.id]: "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913",
   [bsc.id]: "0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d",
-  [bscTestnet.id]: "0x64544969ed7EBf5f083679233325356EbE738930",
 };
 
 export const ACP_CONTRACT_ADDRESSES: Record<number, string> = {
-  [baseSepolia.id]: "0x81b06b2085438cef320f2A91a69f84eF6431b8e6",
-  [bscTestnet.id]: "0x6d8a718Bf031258921A1321d833Cf893B56d6f09",
+  [baseSepolia.id]: "0x0b93793923CD5De81850aF8604a233f3f24d461e",
+  [bscTestnet.id]: "0x0b93793923CD5De81850aF8604a233f3f24d461e",
 };
 
 export const FUND_TRANSFER_HOOK_ADDRESSES: Record<number, string> = {
-  [baseSepolia.id]: "0x17DcAC666DB5958d80Ec99A1d372dd9686237c7C",
-  [bscTestnet.id]: "0xfd966805D55a3445092F0285b25658a22b5f0985",
+  [baseSepolia.id]: "0xaD1d2BB31C40e3D0f14631721Babc4b889F38796",
+  [bscTestnet.id]: "0xaD1d2BB31C40e3D0f14631721Babc4b889F38796",
 };
 
 // ---------------------------------------------------------------------------
